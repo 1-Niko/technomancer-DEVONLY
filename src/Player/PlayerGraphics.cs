319be4e4a710ddc5d728c81d0848ed7be0d8212e
Niko <anemailforagoodgame@gmail.com>
2025-04-23 19:41:39 -0400

    Fur Separated
    it is now glued on (needs work)

--- src/Player/PlayerGraphics.cs
+++ src/Player/PlayerGraphics.cs
@@ -1,32 +1,107 @@
+using UnityEngine.PlayerLoop;
+
 namespace Slugpack;
 
 internal static class PlayerGraphicsHooks
 {
-    private static readonly List<float> FEAFD765 = [64f, 32f, 16f, 8f, 4f, 2f, 1f];
-    private static readonly List<float> D266428E = [-1f, 4f, 2f, 1f, 0.5f, 0.25f, 0.125f, 0.0625f, 0.03125f, 0.015625f, 0.0078125f, 0.00390625f, 0.001953125f];
-    private static readonly List<float> E6AB2708 = [-1f, 8f, 4f, 2f, 1f, 0.5f, 0.25f, 0.125f, 0.0625f, 0.03125f, 0.015625f, 0.0078125f, 0.00390625f, 0.001953125f];
-    private static readonly List<float> CCFC91A1 = [-1f, 256f, 128f, 64f, 32f, 16f, 8f, 4f, 2f, 1f, 0.5f, 0.25f, 0.125f, 0.0625f];
-    private static readonly List<float> EC274780 = [1f, 0.5f, 0.25f, 0.125f, 0.0625f, 0.03125f, 0.015625f, 0.0078125f, 0.00390625f, 0.001953125f];
-    private static readonly List<float> B9B05710 = [-1f, 1f];
+    public class TechnomancerFur : CosmeticSprite
+    {
+        public TechnomancerFur(Room room, PlayerGraphics owner)
+        {
+            this.pos = pos;
+            this.room = room;
+            this.owner = owner;
+        }
+
+        public override void Update(bool eu)
+        {
+        }
+
+        public override void DrawSprites(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, float timeStacker, Vector2 camPos)
+        {
+            sLeaser.sprites[0].scale = 100f;
+            sLeaser.sprites[0].SetPosition(pos);
+            sLeaser.sprites[0].isVisible = !owner.player.inShortcut;
+            base.DrawSprites(sLeaser, rCam, timeStacker, camPos);
+        }
+
+        public override void InitiateSprites(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam)
+        {
+            sLeaser.sprites = new FSprite[1];
+            for (int i = 0; i < 1; i++)
+            { sLeaser.sprites[i] = new FSprite("pixel", true); }
+
+            AddToContainer(sLeaser, rCam, null);
+        }
+
+        public override void AddToContainer(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, FContainer newContatiner)
+        {
+            newContatiner ??= rCam.ReturnFContainer("Midground");
+            foreach (FSprite fsprite in sLeaser.sprites)
+            {
+                fsprite.RemoveFromContainer();
+                newContatiner.AddChild(fsprite);
+            }
+        }
+
+        public Vector2 pos;
+
+        public Room room;
+
+        public PlayerGraphics owner;
+    }
+
+    public class VoyagerFur : CosmeticSprite
+    {
+        public VoyagerFur(Vector2 pos)
+        {
+            this.pos = pos;
+        }
+
+        public override void Update(bool eu)
+        {
+        }
+
+        public override void DrawSprites(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, float timeStacker, Vector2 camPos)
+        {
+            base.DrawSprites(sLeaser, rCam, timeStacker, camPos);
+        }
+
+        public override void InitiateSprites(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam)
+        {
+            sLeaser.sprites = new FSprite[1];
+            for (int i = 0; i < 1; i++)
+            { sLeaser.sprites[i] = new FSprite("pixel", true); }
+
+            AddToContainer(sLeaser, rCam, null);
+        }
+
+        public override void AddToContainer(RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, FContainer newContatiner)
+        {
+            newContatiner ??= rCam.ReturnFContainer("Midground");
+            foreach (FSprite fsprite in sLeaser.sprites)
+            {
+                fsprite.RemoveFromContainer();
+                newContatiner.AddChild(fsprite);
+            }
+        }
+
+        public Vector2 pos;
+    }
 
     internal static void Apply()
     {
         On.PlayerGraphics.Update += PlayerGraphics_Update;
-        //On.PlayerGraphics.InitiateSprites += PlayerGraphics_InitiateSprites;
-        //On.PlayerGraphics.DrawSprites += PlayerGraphics_DrawSprites;
-        //On.PlayerGraphics.AddToContainer += PlayerGraphics_AddToContainer;
+        On.PlayerGraphics.InitiateSprites += PlayerGraphics_InitiateSprites;
+        On.PlayerGraphics.DrawSprites += PlayerGraphics_DrawSprites;
     }
 
     private static void PlayerGraphics_Update(On.PlayerGraphics.orig_Update orig, PlayerGraphics self)
     {
         orig(self);
 
-        if (!self.player.IsTechy(out var scanline)) return;
-
         if (self.player.Consious && self.objectLooker.currentMostInteresting != null && self.objectLooker.currentMostInteresting is Creature)
         {
-            //scanline = ScanLineMemory.GetOrCreateValue(self.player);
-
             CreatureTemplate.Relationship relationship = self.player.abstractCreature.creatureTemplate.CreatureRelationship((self.objectLooker.currentMostInteresting as Creature).abstractCreature.creatureTemplate);
 
             float dangerLevel = Mathf.InverseLerp(Mathf.Lerp(40f, 250f, relationship.intensity), 10f, Vector2.Distance(self.player.mainBodyChunk.pos, self.objectLooker.mostInterestingLookPoint) * (self.player.room.VisualContact(self.player.mainBodyChunk.pos, self.objectLooker.mostInterestingLookPoint) ? 1f : 1.5f));
@@ -34,14 +109,26 @@ internal static class PlayerGraphicsHooks
             {
                 dangerLevel *= (self.objectLooker.currentMostInteresting as Creature).abstractCreature.abstractAI.RealAI.CurrentPlayerAggression(self.player.abstractCreature);
             }
-
-            scanline.danger = dangerLevel;
         }
     }
 
     private static void PlayerGraphics_InitiateSprites(On.PlayerGraphics.orig_InitiateSprites orig, PlayerGraphics self, RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam)
     {
+        if (TechyFur == null)
+        {
+            TechyFur = new TechnomancerFur(self.player.room, self);
+            self.player.room.AddObject(TechyFur);
+        }
+        else if (TechyFur.room != self.player.room)
+        {
+            TechyFur.Destroy();
+            TechyFur = new TechnomancerFur(self.player.room, self);
+            self.player.room.AddObject(TechyFur);
+        }
+
         orig(self, sLeaser, rCam);
+
+        TechyFur.pos = sLeaser.sprites[0].GetPosition();
 
         string slug = self.player.slugcatStats.name.value;
         if (!new List<string> { "voyager", "technomancer" }.Contains(slug))
@@ -66,60 +153,11 @@ internal static class PlayerGraphicsHooks
                 self.tail[3] = new TailSegment(self, 1f, 7f, self.tail[2], 0.85f, 1f, 0.5f, true);
                 break;
         }
-
-        List<BodyPart> list = Enumerable.ToList(self.bodyParts);
-        _ = list.RemoveAll((BodyPart x) => x is TailSegment);
-        list.AddRange(self.tail);
-        self.bodyParts = [.. list];
-
-        string binaryString = ConvertNumbersToBinaryString(FurData, 64);
-        List<string> chunks = SplitStringIntoChunks(binaryString, 102);
-
-        int slugTufts = chunks.Count(chunk => slug == ((chunk[0] == '0') ? "technomancer" : "voyager"));
-
-        Array.Resize(ref sLeaser.sprites, sLeaser.sprites.Length + slugTufts);
-
-        bool furEnabled = OptionsMenu.furToggle.Value;
-        // Plugin.DebugLog($"Current Fur state : {furEnabled}");
-
-        int index = 0;
-        for (int i = 0; i < chunks.Count; i++)
-        {
-            if (slug == ((chunks[i][0] == '0') ? "technomancer" : "voyager") && sLeaser.sprites[index + 13] == null)
-            {
-                sLeaser.sprites[index + 13] = new FSprite($"FurTuft{BinaryToFloat(2, chunks[i], FEAFD765)}", true)
-                {
-                    isVisible = !furEnabled,
-
-                    anchorX = BinaryToFloat(50, chunks[i], EC274780),
-                    anchorY = BinaryToFloat(60, chunks[i], EC274780),
-
-                    scaleX = BinaryToFloat(70, chunks[i], B9B05710),
-                    scaleY = BinaryToFloat(72, chunks[i], B9B05710)
-                };
-
-                index++;
-            }
-        }
-
-        // DEBUG TESTING REMOVE OR COMMENT LATER
-        // for (int i = 0; i < sLeaser.sprites.Length; i++)
-        // {
-        //     if (SlugpackShaders.TryGetValue(self.owner.room.game.rainWorld, out var Shaders))
-        //         sLeaser.sprites[i].shader = Shaders.Redify;
-        // }
-
-        self.AddToContainer(sLeaser, rCam, null);
     }
 
     private static void PlayerGraphics_DrawSprites(On.PlayerGraphics.orig_DrawSprites orig, PlayerGraphics self, RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, float timeStacker, Vector2 camPos)
     {
         orig(self, sLeaser, rCam, timeStacker, camPos);
-
-        if (self.player.IsTechy(out var scanline))
-        {
-            //Leave this empty, you have 2 slugcats here if you check for 1 the other one will miss the sprites 
-        }
 
         string slug = self.player.slugcatStats.name.value;
         if (!new List<string> { "voyager", "technomancer" }.Contains(slug))
@@ -143,78 +181,9 @@ internal static class PlayerGraphicsHooks
         if (!OptionsMenu.furToggle.Value && sLeaser.sprites[3]?.element?.name is string text && text.StartsWith("Head"))
             sLeaser.sprites[3].element = Futile.atlasManager.GetElementWithName("Fluff" + text);
 
-        // scanline = ScanLineMemory.GetOrCreateValue(self.player);
-
-        scanline.screenPosition = new Vector2(((self.owner.abstractPhysicalObject.realizedObject as Creature).mainBodyChunk.pos.x - rCam.pos.x + 19) / 1400f, ((self.owner.abstractPhysicalObject.realizedObject as Creature).mainBodyChunk.pos.y - rCam.pos.y + 72) / 900f);
-
-        string binaryString = ConvertNumbersToBinaryString(FurData, 64);
-        List<string> chunks = SplitStringIntoChunks(binaryString, 102);
-
-        int index = 0;
-        for (int i = 0; i < chunks.Count; i++)
-        {
-            if (slug == ((chunks[i][0] == '0') ? "technomancer" : "voyager") && sLeaser.sprites[index + 13] != null)
-            {
-                float offsetX = BinaryToFloat(9, chunks[i], D266428E);
-                float offsetY = BinaryToFloat(22, chunks[i], E6AB2708);
-
-                sLeaser.sprites[index + 13].SetPosition(RotateAroundPoint(sLeaser.sprites[int.Parse(chunks[i][1].ToString())].GetPosition(), MultiplyVector2ByFloats(new Vector2(offsetX, offsetY), sLeaser.sprites[int.Parse(chunks[i][1].ToString())].scaleX, sLeaser.sprites[int.Parse(chunks[i][1].ToString())].scaleY), -sLeaser.sprites[int.Parse(chunks[i][1].ToString())].rotation));
-
-                float defaultRotation = BinaryToFloat(36, chunks[i], CCFC91A1);
-                float minimumRotation = BinaryToFloat(74, chunks[i], CCFC91A1);
-                float maximumRotation = BinaryToFloat(88, chunks[i], CCFC91A1);
-
-                sLeaser.sprites[index + 13].rotation = sLeaser.sprites[int.Parse(chunks[i][1].ToString())].rotation + CalculateFurRotation(defaultRotation, minimumRotation, maximumRotation, scanline.wetness, scanline.danger, num);
-
-                sLeaser.sprites[index + 13].isVisible = !OptionsMenu.furToggle.Value;
-
-                index++;
-            }
-        }
+        TechyFur.pos = sLeaser.sprites[0].GetPosition();
     }
 
-    private static void PlayerGraphics_AddToContainer(On.PlayerGraphics.orig_AddToContainer orig, PlayerGraphics self, RoomCamera.SpriteLeaser sLeaser, RoomCamera rCam, FContainer newContatiner)
-    {
-        orig(self, sLeaser, rCam, newContatiner);
-        string slug = self.player.slugcatStats.name.value;
-        if (!new List<string> { "voyager", "technomancer" }.Contains(slug))
-            return;
 
-        string binaryString = ConvertNumbersToBinaryString(FurData, 64);
-        List<string> chunks = SplitStringIntoChunks(binaryString, 102);
-
-        int slugTufts = chunks.Count(chunk => slug == ((chunk[0] == '0') ? "technomancer" : "voyager"));
-
-        FContainer midground = rCam.ReturnFContainer("Midground");
-        FContainer foreground = rCam.ReturnFContainer("Foreground");
-
-        // Ensure both containers are available before proceeding.
-        if (midground == null || foreground == null)
-        {
-            Plugin.DebugError("Midground or Foreground container is null in PlayerGraphics_AddToContainer");
-            return;
-        }
-
-        // Ensure the sprite array exists and is large enough.
-        if (sLeaser.sprites != null && sLeaser.sprites.Length >= slugTufts + 13)
-        {
-            for (int i = 0; i < slugTufts; i++)
-            {
-                int targetIndex = i + 13;
-                if (targetIndex >= sLeaser.sprites.Length || sLeaser.sprites[targetIndex] == null)
-                    continue;
-
-                // Remove the sprite from the foreground container and add it to the midground container.
-                foreground.RemoveChild(sLeaser.sprites[targetIndex]);
-                midground.AddChild(sLeaser.sprites[targetIndex]);
-
-                // Parse the secondary index safely.
-                if (chunks[i].Length > 1 && int.TryParse(chunks[i][1].ToString(), out int otherIndex)
-                    && otherIndex < sLeaser.sprites.Length && sLeaser.sprites[otherIndex] != null)
-                {
-                    sLeaser.sprites[targetIndex].MoveBehindOtherNode(sLeaser.sprites[otherIndex]);
-                }
-            }
-        }
-    }
+    private static TechnomancerFur TechyFur;
 }
